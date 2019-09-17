8ca97b2c761826017bbad71d3e16c4bbd99084ab
wanghc <haochen94@aliyun.com>
2019-09-17 09:28:46 +0800

    通道分析

--- src/views/FinanceManage/Analysis/Analysis.vue
+++ src/views/FinanceManage/Analysis/Analysis.vue
@@ -1,6 +1,25 @@
 <template>
     <div>
-        通道分析
+        <div class="analysis_classify">
+            <p><img src="" alt="">微信公众号-901</p>
+            <div class="analysis_text">
+                <p>交易金额: 0元</p>
+                <p>手续费: 0元</p>
+                <p>入金金额: 0元</p>
+            </div>
+            <div class="analysis_footer">
+                <p>
+                    <span>交易笔数: 0</span>
+                    <span>失败率: 0%</span>
+                </p>
+                <p>
+                    <span>成功笔数: 0</span>
+                    <span>失败笔数: 0</span>
+                </p>
+            </div>
+        </div>
     </div>
 </template>
 <script src="./Analysis.js"></script>
+<style scoped lang="scss"  src="./Analysis.scss"></style>
+
